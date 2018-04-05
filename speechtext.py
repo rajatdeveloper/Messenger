2d1fd08cfeddc13c13060de8eba54e8da734a53a
rajatdeveloper <singhal.rajat97@gmail.com>
2018-04-05 22:45:56 +0530

    mail sending function added

--- speechtext.py
+++ speechtext.py
@@ -1,12 +1,25 @@
 import string
 import speech_recognition as sr
-
+import smtplib
+import xlrd
+import datetime
+import ctypes
+sender = input()
+passward = input()
+receiver = input()
+mail = smtplib.SMTP()
+mail.connect('smtp.gmail.com', 587)
+mail.ehlo()
+mail.starttls()  # use to make secure connection
+mail.login(sender,passward)
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
     try:
         print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        mail.sendmail(sender,receive,r.recognize_google(audio))
     except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
+mail.close()
